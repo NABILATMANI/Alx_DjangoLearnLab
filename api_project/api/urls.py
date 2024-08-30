78afcdacdeaee935e8b5b0021dbbbc60efa08bef
NABILATMANI <atmaninabile@gmail.com>
2024-08-30 23:47:15 +0100

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -4,3 +4,13 @@ from .views import BookList
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
 ]
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
