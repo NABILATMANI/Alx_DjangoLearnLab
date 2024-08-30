ce735b70791b2f50a838fbe4a7f88878e488d067
NABILATMANI <atmaninabile@gmail.com>
2024-08-30 22:45:32 +0100

    Add files via upload

---
+++ api_project/api/__init__.py
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# This file is intentionally left blank. It indicates that this directory should be treated as a Python package.
+=======
+# migrations/__init__.py
+
+# This file can be left empty
+>>>>>>> origin/main
