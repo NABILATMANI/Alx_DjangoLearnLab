2bd8a7fc213b5ece6406f9e6fcfdf5975cdbb46d
NABILATMANI <atmaninabile@gmail.com>
2024-08-30 23:01:42 +0100

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -21,3 +21,9 @@ from .views import BookList
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
 ]
+from django.urls import path
+from .views import BookList
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+]
