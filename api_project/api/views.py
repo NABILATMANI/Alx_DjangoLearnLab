b5e2a91b5303b2efb7f7c3c7d41d750e57b2984f
NABILATMANI <atmaninabile@gmail.com>
2024-08-30 23:06:34 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,28 +1,16 @@
 from rest_framework.generics import ListAPIView
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
-# Task 1: Building Your First API Endpoint
+# For Task 1: Building Your First API Endpoint
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-# Task 2: Implementing CRUD Operations with ViewSets and Routers
+# For Task 2: Implementing CRUD Operations
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-from rest_framework.generics import ListAPIView
-from .models import Book
-from .serializers import BookSerializer
-
-class BookList(ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-from rest_framework.generics import ListAPIView
-from .models import Book
-from .serializers import BookSerializer
-
-class BookList(ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
