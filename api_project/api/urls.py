5e2ef5e8a7a30aa3c7705d1c4f4d555b717b2aeb
NABILATMANI <atmaninabile@gmail.com>
2024-08-30 23:47:37 +0100

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -14,3 +14,9 @@ router.register(r'books', BookViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+]
