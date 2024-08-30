2c036f364d48bfabf123b022fc0f7fffb23a06f6
NABILATMANI <atmaninabile@gmail.com>
2024-08-30 22:57:57 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -12,3 +12,10 @@ class BookList(ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+from rest_framework.generics import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
