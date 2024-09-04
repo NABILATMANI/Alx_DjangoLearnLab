0dfe04754d16bae2166bea25e7d760f02b8dc39e
NABILATMANI <atmaninabile@gmail.com>
2024-09-04 21:42:20 +0100

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from .views import BookListView, BookDetailView
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
