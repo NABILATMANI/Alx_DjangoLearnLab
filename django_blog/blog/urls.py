98382ae872b590010f7a12e5eef4f328105e42c5
NABILATMANI <atmaninabile@gmail.com>
2024-09-12 00:20:21 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -41,3 +41,13 @@ urlpatterns = [
     path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),  # Ensure this line is present
     path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('posts/', views.PostListView.as_view(), name='post_list'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+]
